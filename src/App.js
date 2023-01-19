be516b59bad01095b901b6359a75ec01f09da5cf
Adriel Klein <adrielmklein@gmail.com>
2023-01-19 14:57:43 -0600

    add title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const App = () => {
-  const [userInput, setUserInput] = useState('');
-  const [poem, setPoem] = useState('');
+  const [userInput, setUserInput] = useState("");
+  const [poem, setPoem] = useState("");
 
-  const handleChange = event => {
+  const handleChange = (event) => {
     setUserInput(event.target.value);
   };
 
@@ -17,15 +17,22 @@ const App = () => {
   };
 
   return (
-    <div style={{textAlign: 'center'}}>
+    <div style={{ textAlign: "center" }}>
       <h1>Poem Generator</h1>
-      <input type="text" onChange={handleChange} value={userInput} />
+      <input
+        type="text"
+        onChange={handleChange}
+        value={userInput}
+        style={{
+          margin: "8px 4px",
+          boxSizing: "border-box",
+          borderRadius: "4px",
+        }}
+      />
       <button onClick={generatePoem}>Generate Poem</button>
       <div>{poem}</div>
     </div>
   );
-}
-
-
+};
 
 export default App;
